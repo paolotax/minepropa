b00a4251dd2c919113e91e1d187a3fbe149c03a2
Paolo <paolo.tassinari@gmail.com>
2011-03-30 21:50:56 +0200

    directions

--- app/controllers/visite_controller.rb
+++ app/controllers/visite_controller.rb
@@ -3,7 +3,7 @@ class VisiteController < ApplicationController
   
   def index
     
-    @visite = Visita.where('visite.start >= ?', Time.at(params[:start].to_f)).where('visite.end <= ?', Time.at(params[:end].to_f))
+    @visite = Visita.where('visite.start >= ?', Time.at(1301436000)).where('visite.end <= ?', Time.at(1301522400))
     
     @data = []
     @visite.each do |e|
