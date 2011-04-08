746aa404b93c6de8b354aaa44566101b90af0ddd
Paolo <paolo.tassinari@gmail.com>
2011-04-08 21:31:22 +0200

    maps controller

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -1,2 +1,19 @@
 module MapsHelper
+ 
+ 
+  # ORRENDO CICLO 
+  def get_scuola_latlng(scuola_id)
+    @indirizzo = Indirizzo.where('indirizzi.indirizzable_id = ? AND indirizzi.indirizzable_type = ?', scuola_id, "Scuola").first
+    
+    if (!@indirizzo.nil?)
+      if (!@indirizzo.latitude.nil?) && (!@indirizzo.longitude.nil?)
+        return @indirizzo.latitude + ',' + @indirizzo.longitude 
+      else
+        return ''
+      end
+    else
+      return ''
+    end
+  end
+    
 end
