ffb7356f198bd5f7658202b3bcf9821f2a035e54
Paolo <paolo.tassinari@gmail.com>
2011-05-24 00:04:30 +0200

    correzione per heroku??

--- app/models/appunto_riga.rb
+++ app/models/appunto_riga.rb
@@ -48,7 +48,7 @@ class AppuntoRiga < ActiveRecord::Base
     re = /([c|o])|([\d,]+)|(-[\d,]+)/
     m = re.match pr
     if m[1] == 'c'
-      self.unitario = "15,00"
+      self.unitario = self.libro.prezzo_consigliato / 100
     else
       if m[1] == 'o'
         self.unitario = "0"
