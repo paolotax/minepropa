8a5d4a36d4db575b4b448dda2d8d454118e4b8e5
paolotax <paolo.tassinari@gmail.com>
2011-05-27 15:00:42 +0200

    pdf

--- app/models/appunto_riga.rb
+++ app/models/appunto_riga.rb
@@ -25,7 +25,7 @@ class AppuntoRiga < ActiveRecord::Base
   scope :consegnati,    where("appunto_righe.consegnato = true")
   scope :pagati,        where("appunto_righe.pagato = true")
   scope :da_pagare,     where("appunto_righe.pagato = false OR appunto_righe.pagato IS NULL")
-  scope :per_id,        order(:libro_id)
+  scope :per_libro_id,        order(:libro_id)
   
   composed_of :unitario,
       :class_name  => "Money",
