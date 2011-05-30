0976dfd511568fb5b6bad4f77445ec62720c78de
Paolo <paolo.tassinari@gmail.com>
2011-05-30 18:54:25 +0200

    appunro_riga

--- app/presenters/libri/show_presenter.rb
+++ app/presenters/libri/show_presenter.rb
@@ -7,23 +7,23 @@ class Libri::ShowPresenter
   end
   
   def da_consegnare
-    @libro.appunto_righe.da_consegnare
+    @libro.appunto_righe.da_consegnare.per_id
   end
   
   def da_pagare
-    @libro.appunto_righe.da_pagare
+    @libro.appunto_righe.da_pagare.per_id
   end
   
   def consegnati
-    @libro.appunto_righe.consegnati
+    @libro.appunto_righe.consegnati.per_id
   end
   
   def consegnati_da_pagare
-    @libro.appunto_righe.consegnati.da_pagare
+    @libro.appunto_righe.consegnati.da_pagare.per_id
   end
   
   def consegnati_pagati
-    @libro.appunto_righe.consegnati.pagati
+    @libro.appunto_righe.consegnati.pagati.per_id
   end
   
   memoize :da_consegnare, :da_pagare, :consegnati, :consegnati_da_pagare, :consegnati_pagati
