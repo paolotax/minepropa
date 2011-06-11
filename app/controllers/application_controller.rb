9f0070badf429a395ce5f0421c8640c997df4f49
Paolo <paolo.tassinari@gmail.com>
2011-06-11 21:32:55 +0200

    status class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,43 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper :all
-  layout 'new_layout'
   
   before_filter :prepare_for_mobile
   before_filter :instantiate_controller_and_action_names
 
+  layout :layout_by_resource
+
+  protected
+
+    def layout_by_resource
+      if devise_controller?
+        "devise_layout"
+      else
+        "new_layout"
+      end
+    end
+
   
   private
 
-  def mobile_device?
-    if session[:mobile_param]
-      session[:mobile_param] == "1"
-    else
-      request.user_agent =~ /Mobile|webOS|Android/
+    def mobile_device?
+      if session[:mobile_param]
+        session[:mobile_param] == "1"
+      else
+        request.user_agent =~ /Mobile|webOS|Android/
+      end
     end
-  end
   
-  helper_method :mobile_device?
+    helper_method :mobile_device?
 
-  def prepare_for_mobile
-    session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device?
-  end
+    def prepare_for_mobile
+      session[:mobile_param] = params[:mobile] if params[:mobile]
+      request.format = :mobile if mobile_device?
+    end
 
-  def instantiate_controller_and_action_names
-    @current_action = action_name
-    @current_controller = controller_name
-  end
+    def instantiate_controller_and_action_names
+      @current_action = action_name
+      @current_controller = controller_name
+    end
 
 end
