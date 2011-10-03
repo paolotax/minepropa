7012db179b7ee494dfe57fd9e7fe761575b9c54b
paolotax <paolo.tassinari@gmail.com>
2011-10-03 08:43:07 +0200

    appunto form

--- app/controllers/appunti_controller.rb
+++ app/controllers/appunti_controller.rb
@@ -41,6 +41,7 @@ class AppuntiController < ApplicationController
 
   def new
     @appunto   = current_user.appunti.build
+    @appunto.scuola = Scuola.find(params[:scuola]) if params[:scuola]
     @indirizzo = @appunto.indirizzi.build
     #@recent    = Appunto.modificato_dal(Time.now.at_beginning_of_day, Time.now.at_beginning_of_day).per_id
   end
